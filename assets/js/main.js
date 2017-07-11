0b1f44c91fc8363e84d012fa0f0356ad58d4abbb
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-11 13:13:39 -0400

    overlay estilo

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 				console.log(titulo);
 				$(".addp").append("<div class='caja-blanca'><img src='"+e.images.normal+"'><div class='overlay'><div class='text'>Hello World</div></div><br><i class='fa fa-eye' aria-hidden='true'></i>"+e.views_count+"<i class='fa fa-comment' aria-hidden='true'></i>"+e.comments_count+"<i class='fa fa-heart' aria-hidden='true'></i>"+e.likes_count+"</div>")
 			});
+			userName();
 	});
 		
 
@@ -14,7 +15,7 @@ $(document).ready(function() {
 		
 		var nameUser = "RypeArts";
 		console.log(nameUser);
-		$(".titulousuario-name").append("<h4>" + nameUser + "</h4>");
+		$(".titulousuario-name").append("<a href='#'><h4>" + nameUser + "</h4></a>");
 	}
 	
 });
