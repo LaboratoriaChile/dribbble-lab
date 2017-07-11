0de38d5bb031e417ca7ab3933c69b564cc3c6c91
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-11 12:03:15 -0400

    get json listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var user = 'RypeArts';
+	/*var user = 'RypeArts';
 	var token = 'a94cc9a5cdd89d403fe031a7173778db76ebd7ed8d919fbc2efc33371723ecea';
 
 	$.ajax({
@@ -20,7 +20,15 @@ $(document).ready(function() {
 		console.log("complete");
 	});
 
+*/
 
+	$.getJSON('https://api.dribbble.com/v1/users/RypeArts/shots?access_token=a94cc9a5cdd89d403fe031a7173778db76ebd7ed8d919fbc2efc33371723ecea', function(data) {
+			data.forEach(function(e){
+				var titulo = e.title;
+				console.log(titulo);
+				$(".addp").append("<h2 class='name'>"+titulo+"</h2>")
+			})
+	});
 	var userName = function (data){
 		
 		var nameUser = "RypeArts";
