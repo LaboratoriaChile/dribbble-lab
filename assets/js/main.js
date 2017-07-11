e571c0b7982316728cdd656fa16566232d5db949
genarvaez <ge_naravez@hotmail.com>
2017-07-11 12:16:43 -0400

    maqueta de proyectos lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,29 @@ $(function() {
     	if (resp.data.length > 0) {
     	console.log(resp);						
       	$.each(resp.data.reverse(), function(i, val) {
-        $('#dribbble').prepend(
-          '<li class="box"><img src="'+val.images.normal+'" /><h2>'+val.title+'</h2><p><a href="'+val.html_url+'">original post &rarr;</a></p></li><li class="comments">'+val.comments_count+'</li><li class="likes">'+val.likes_count+'</li>'+'<li class="views">'+val.views_count+'</li>'
-        )
+        $('.project').prepend('<div class="col-md-4 box-project"><img class="img-responsive" src="'+val.images.normal+'"/><span>'+val.views_count+'</span>'+'<span><i class="fa fa-eye" aria-hidden="true"></i>'+val.comments_count+'</span><span><i class="fa fa-comment-o" aria-hidden="true"></i>'+val.comments_count+'<i class="fa fa-heart" aria-hidden="true"></i></span></div>')
       });
     	}else{
-      		$('#dribbble').append('<li>No shots.</li>');
+      		$('.project').append('<li>No shots.</li>');
     		}
   		});		   							    
 	});
+/*
+
+'<li class="box"><img src="'+val.images.normal+'" /><h2>'+val.title+'</h2><p><a href="'+val.html_url+'">original post &rarr;</a></p></li><li class="comments">'+val.comments_count+'</li><li class="likes">'+val.likes_count+'</li>'+'<li class="views">'+val.views_count+'</li>'
+
+
+$(function() {     
+      $.getJSON('https://api.dribbble.com/v1/users/simplebits', function(resp) {
+      if (resp.data.length > 0) {
+      console.log(resp);            
+        $.each(resp.data.reverse(), function(i, val) {
+        $('.img-project').prepend(
+          '<li class="box"><img src="'+val.images.normal+'" /><h2>'+val.title+'</h2><p><a href="'+val.html_url+'">original post &rarr;</a></p></li><li class="comments">'+val.comments_count+'</li><li class="likes">'+val.likes_count+'</li>'+'<li class="views">'+val.views_count+'</li>'
+        )
+      });
+      }else{
+          $('.img-project').append('<li>No shots.</li>');
+        }
+      });                       
+  });*/
