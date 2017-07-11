a01df97737fe546609956f1e650b7f19505b5fdb
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-11 12:15:40 -0400

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
-	var user = 'eduardo';
-	var token = 'a94cc9a5cdd89d403fe031a7173778db76ebd7ed8d919fbc2efc33371723ecea';
 
-	$.ajax({
-
-		url: 'https://api.dribbble.com/v1/users/'+ user+'/shots?access_token='+token,
-		type: 'GET',
-		dataType: 'json',
-		
-	})
-	.done(function() {
-		console.log("success");
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
+	$.getJSON('https://api.dribbble.com/v1/users/RypeArts/shots?access_token=a94cc9a5cdd89d403fe031a7173778db76ebd7ed8d919fbc2efc33371723ecea', function(data) {
+			data.forEach(function(e){
+				var titulo = e.title;
+				console.log(titulo);
+				$(".addp").append("<h2 class='name'>"+titulo+"</h2>")
+			})
 	});
+	var userName = function (data){
+		
+		var nameUser = "RypeArts";
+		console.log(nameUser);
+		$(".titulousuario-name").append("<h4>" + nameUser + "</h4>");
+	}
 	
 });
+
+var addLikes = function(array){
+
+	array.forEach(function(e){
+		var titulo = e.title;
+		$(".addp").append('<div class="name">'+titulo+'</div>')
+	})
+
+};
