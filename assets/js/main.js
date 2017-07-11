e0ae1b9f3b2e39dfaf072364b4aa6355ad2d7a6e
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-11 13:29:15 -0400

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+
+	/*var user = 'RypeArts';
+	var token = 'a94cc9a5cdd89d403fe031a7173778db76ebd7ed8d919fbc2efc33371723ecea';
+
+
 	$.getJSON('https://api.dribbble.com/v1/users/RypeArts/shots?access_token=a94cc9a5cdd89d403fe031a7173778db76ebd7ed8d919fbc2efc33371723ecea', function(data) {
 			data.forEach(function(e){
 				var titulo = e.title;
@@ -7,7 +12,37 @@ $(document).ready(function() {
 				$(".addp").append("<div class='box col-md-3'><img class='img-responsive imges' src='"+e.images.normal+"'><br><div class='contadores'><div class='cont'><i class='fa fa-eye icont' aria-hidden='true'></i>"+e.views_count+"</div><div class='cont'><i class='fa fa-comment icont' aria-hidden='true'></i>"+e.comments_count+"</div><div class='cont'><i class='fa fa-heart icont' aria-hidden='true'></i>"+e.likes_count+"</div></div></div>")
 			});
 
+
 	});
+
+		url: 'https://api.dribbble.com/v1/users/'+ user+'/shots?access_token='+token,
+		type: 'GET',
+		dataType: 'json',
+		
+	})
+	.done(function(respuesta) {
+		console.log("success");
+		console.log(respuesta)
+		addLikes(respuesta.results)
+		userName();
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+
+*/
+
+	$.getJSON('https://api.dribbble.com/v1/users/RypeArts/shots?access_token=a94cc9a5cdd89d403fe031a7173778db76ebd7ed8d919fbc2efc33371723ecea', function(data) {
+			data.forEach(function(e){
+				var titulo = e.title;
+				console.log(titulo);
+				$(".addp").append("<h2 class='name'>"+titulo+"</h2>")
+			})
+	});
+
 	var userName = function (data){
 		
 		var nameUser = "RypeArts";
@@ -16,3 +51,16 @@ $(document).ready(function() {
 	}
 	
 });
+
+
+
+var addLikes = function(array){
+
+	array.forEach(function(e){
+		var titulo = e.title;
+		$(".addp").append('<div class="name">'+titulo+'</div>')
+	})
+
+};
+
+
