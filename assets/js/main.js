e158d2ce6b297c2d9b35edaa60622b52bc23066a
JosefaHerrera <josefa@linuxero.cl>
2017-07-11 14:17:49 -0400

    five commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
 			data.forEach(function(e){
 				var titulo = e.title;
 				console.log(titulo);
-				$(".addp").append("<div class='box col-md-3'><img class='img-responsive imges' src='"+e.images.normal+"'><br><div class='contadores'><div class='cont'><i class='fa fa-eye icont' aria-hidden='true'></i>"+e.views_count+"</div><div class='cont'><i class='fa fa-comment icont' aria-hidden='true'></i>"+e.comments_count+"</div><div class='cont'><i class='fa fa-heart icont' aria-hidden='true'></i>"+e.likes_count+"</div></div></div>")
+				$(".addp").append("<div class='box col-md-3'><img class='img-responsive imges' src='"+e.images.normal+"'><div class='overlay'><div class='text'>"+e.description+"</div></div><br><div class='contadores'><div class='cont'><i class='fa fa-eye icont' aria-hidden='true'></i>"+e.views_count+"</div><div class='cont'><i class='fa fa-comment icont' aria-hidden='true'></i>"+e.comments_count+"</div><div class='cont'><i class='fa fa-heart icont' aria-hidden='true'></i>"+e.likes_count+"</div></div></div>")
 			});
 			userName();
 	});
