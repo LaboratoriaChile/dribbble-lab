7f63059e59c112e527a33e698b53f38d0a3cd3f4
aaliaga <a.aliaga.val@gmail.com>
2017-07-11 13:22:45 -0400

    header contadores listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,13 @@ $(document).ready(function() {
 
 	$.getJSON('https://api.dribbble.com/v1/users/RypeArts/shots?access_token=a94cc9a5cdd89d403fe031a7173778db76ebd7ed8d919fbc2efc33371723ecea', function(data) {
 			data.forEach(function(e){
-				var shotCont = e.shots_count;
-				console.log(shotCont);
-				$(".shots").append(shotCont)
+				console.log(data.buckets_count);
+
 			});
 	});
 });
 
-/*
+
 	var userName = function (data){
 		
 		var nameUser = "RypeArts";
@@ -17,8 +16,7 @@ $(document).ready(function() {
 		$(".titulousuario-name").append("<h4>" + nameUser + "</h4>");
 	}
 	
-});
-
+/*
 var addLikes = function(array){
 
 	array.forEach(function(e){
