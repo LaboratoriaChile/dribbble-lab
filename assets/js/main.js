ef815110fc7279594ef717211c6641e765c5243a
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-11 10:42:51 -0400

    json listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
-/* Acá va todo el jquery */
+$(document).ready(function() {
+	var token = 'a94cc9a5cdd89d403fe031a7173778db76ebd7ed8d919fbc2efc33371723ecea';
+    var user = 'eduardo';
+       //num_photos = 6;
+
+	$.ajax({
+		url: 'https://api.dribbble.com/v1/users/'+user+'/shots?access_token=' + token,
+		type: 'GET',
+		dataType: 'json',
+		
+	})
+	.done(function() {
+		console.log("success");
+		console.log("")
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+	
+});
