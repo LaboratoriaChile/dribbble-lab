88365b66b6642398db0aaa083b7bd63fa662dc45
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-11 11:51:16 -0400

    llamando nombre

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var user = 'eduardo';
+	var user = 'RypeArts';
 	var token = 'a94cc9a5cdd89d403fe031a7173778db76ebd7ed8d919fbc2efc33371723ecea';
 
 	$.ajax({
@@ -9,8 +9,9 @@ $(document).ready(function() {
 		dataType: 'json',
 		
 	})
-	.done(function() {
+	.done(function(respuesta) {
 		console.log("success");
+		userName();
 	})
 	.fail(function() {
 		console.log("error");
@@ -18,5 +19,13 @@ $(document).ready(function() {
 	.always(function() {
 		console.log("complete");
 	});
+
+
+	var userName = function (data){
+		
+		var nameUser = "RypeArts";
+		console.log(nameUser);
+		$(".titulousuario-name").append("<h4>" + nameUser + "</h4>");
+	}
 	
 });
